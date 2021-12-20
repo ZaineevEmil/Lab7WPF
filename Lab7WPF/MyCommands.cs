1b24cf021087af5bc31836b330fcdcab78a0a6ff
2299112@gmail.com <2299112@gmail.com>
2021-12-21 01:21:16 +0500

    Добавьте файлы проекта.

---
+++ Lab7WPF/MyCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab7WPF
+{
+    class MyCommands
+    {
+        public static RoutedCommand Color { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.B, ModifierKeys.Control, "Ctrl+B"));
+            Color = new RoutedCommand("Color", typeof(MyCommands), inputs);
+        }
+
+    }
+}
